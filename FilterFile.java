719b47159f1ebed8c4e55cefc1dc827274b868c8
angiefoo <fuqianyi@gmail.com>
2015-07-22 16:06:46 +0900

    Update FilterFile.java

--- FilterFile.java
+++ FilterFile.java
@@ -17,10 +17,13 @@ public class FilterFile2 {
     private static final String REGEX =
             "svcAgent";
     
-	public static void main(String[] args) throws IOException {
+    private static final Pattern p = Pattern.compile(REGEX); 
+    
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	        String filename = "src/angie_phonehome_01/sample/diagnostics_australia_2015_07_20_021303.csv.gz";
+	        String filename = "diagnostics_australia_2015_07_20_021303.csv.gz";
 	        Charset charset = Charset.forName("US-ASCII");
+	        
 	        InputStream fileStream = null;
 	        
 			try {
@@ -30,21 +33,43 @@ public class FilterFile2 {
 				e.printStackTrace();
 			}
 
-		InputStream gzipStream = new GZIPInputStream(fileStream);
-	        Reader decoder = new InputStreamReader(gzipStream, charset);
-	        BufferedReader buffered = new BufferedReader(decoder);
-	        
-	        String line;
-			while ((line = buffered.readLine()) != null) {
-		        //System.out.println(line);
-		        
-		        Pattern p = Pattern.compile(REGEX);           
-	            Matcher m = p.matcher(line);
-	            while(m.find()) {
-	            	System.out.println(line);
-	            }
-
+			try {
+				
+			} finally {
+				
 			}
+			
+			BufferedReader buffered = null;
+			
+			try {
+				InputStream gzipStream = new GZIPInputStream(fileStream);
+				Reader decoder = new InputStreamReader(gzipStream, charset);
+		        buffered = new BufferedReader(decoder);
+		        
+		        String line;
+				while ((line = buffered.readLine()) != null) {
+			        //System.out.println(line);
+			                
+		            Matcher m = p.matcher(line);
+		            while(m.find()) {
+		            	System.out.println(line);
+		            }
+
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				if (buffered != null){
+					try {
+						buffered.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+	        
 
 	}
 }
